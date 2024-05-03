3f2078ceb722ab2bbc506cc625963eba41329b5e
Fuyao Tang <tfuyao@outlook.com>
2024-05-03 23:31:26 +0200

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -3,15 +3,25 @@ import click
 
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy # import SQLAlchemy 扩展类
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')      # 设置数据库 URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
-db= SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+db= SQLAlchemy(app)  # initializes the SQLAlchemy extension,allows SQLAlchemy to understand and modify the application's configuration settings related to the database.
+login_manager = LoginManager(app)  # 实例化扩展类
+
+@login_manager.user_loader
+def load_user(user_id):  # find a user given a user ID, when the user logs in or upon each request
+    user = User.query.get(int(user_id))  # 查询用 ID 作为 User 模型的主键对应的用户
+    
+    return user  # 返回用户对象
+
+login_manager.login_view = 'login'  # 设置登录页面的端点 configures Flask-Login to redirect unauthenticated users to the login page
 
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
@@ -52,14 +62,47 @@ def forge():
     db.session.commit()
     click.echo('Done.')  # output information
 
+@app.cli.command()         # 生成管理员账户
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()     #create all the tables and database structure
 
-class User(db.Model): # table name will be user, model class inherits from db.Model
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # 设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # 设置密码
+        db.session.add(user)
+
+    db.session.commit()  # 提交数据库会话
+    click.echo('Done.')
+
+
+
+
+class User(db.Model, UserMixin): # table name will be user, model class inherits from db.Model
     id = db.Column(db.Integer, primary_key=True)  # primary key
     name = db.Column(db.String(20))  # name
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):  # 用于验证密码的方法，接受密码作为参数
+        return check_password_hash(self.password_hash, password)  # 返回布尔值
+
 class Movie(db.Model):  # table name will be movie
     id = db.Column(db.Integer, primary_key=True)  # primary key
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
+
 
 
 @app.context_processor      # register a context processor function
@@ -73,10 +116,13 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index(): #define a view function 索引（首页）
     # user = User.query.first() # get the first user
     if request.method == 'POST':  # 判断是否是 POST 请求
+        if not current_user.is_authenticated:  # 如果当前用户未认证
+            return redirect(url_for('index'))
         # 获取表单数据
         title = request.form.get('title')  # 传入表单对应输入字段的 name 值
         year = request.form.get('year')
@@ -93,8 +139,64 @@ def index(): #define a view function 索引（首页）
     movies = Movie.query.all() # get all movies
     return render_template('index.html', movies=movies) #pass the value of user and movies to the template
 
+@app.route('/settings', methods=['GET', 'POST'])  # 编辑用户设置
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        # current_user 会返回当前登录用户的数据库记录对象
+        # 等同于下面的用法
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first() # retrieve the first user
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登入用户
+            flash('Login success.')
+            return redirect(url_for('index'))  # 重定向到主页
+
+        flash('Invalid username or password.')  # 如果验证失败，显示错误消息
+        return redirect(url_for('login'))  # 重定向回登录页面
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required  # 用于视图保护，后面会详细介绍
+def logout():
+    logout_user()  # 登出用户
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # 重定向回首页
+
+
+
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])              # 编辑电影条目
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -115,6 +217,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)  # 如果是get， 传入被编辑的电影记录
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+@login_required  # 登录保护
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
     db.session.delete(movie)  # 删除对应的记录
