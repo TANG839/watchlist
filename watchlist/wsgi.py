690f8418849be256c4e373b49775f790efa44182
Fuyao Tang <tfuyao@outlook.com>
2024-05-06 23:35:25 +0200

    ready to deploy

---
+++ watchlist/wsgi.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)        # Load environment variables from .env file
+
+from watchlist import app           # Import the app object from the watchlist package
